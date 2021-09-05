861f18d36806147726ba5b9f9d7535df7b325193
riyadhalali <eng.riyadalali93@gmail.com>
2021-09-05 22:38:39 +0300

    signing in application

--- lib/main.dart
+++ lib/main.dart
@@ -49,17 +49,15 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           SingleChildScrollView(
             child: Container(
               width: MediaQuery.of(context).size.width * 100,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15.0)),
+              decoration:
+                  BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(15.0)),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
                   children: [
                     Text(
                       "Disclaimer ",
-                      style: TextStyle(
-                          fontSize: 25.0, fontWeight: FontWeight.bold),
+                      style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
                     ),
                     Text('The TAVRizer App has been developed as a reference, a calculator and a guide only and no representations or guarantees as to the' +
                         'completeness, accuracy or adequacy of any of the information contained in the TAVRizer. The developers do not guarantee the App' +
@@ -67,8 +65,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                         'The developers of the TAVRizer App shall not be liable for any claims or losses arising from the use or misuse of the TAVRizer App, its contents, any omissions from its contents, or otherwise. It is solely designed to be a calculator and it’s output is dependent on the user’s input and user’s accuracy of obtaining the raw information. '),
                     Text(
                       'About',
-                      style: TextStyle(
-                          fontSize: 25.0, fontWeight: FontWeight.bold),
+                      style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
                     ),
                     Text(
                         'The TAVRizer App (TAVR Sizer) is an idea by Mohammed Qintar, MD (Strucutral Interventional Cardiologist at Sparrow Hospital / Michigan State University in Lansing, MI). It aims to help TAVR team members calculate; with ease, the over/under sizing percent of the Edwards Sapien valve in relation to an individual patient’s aortic valve annulus; based on the volume (ml) injected into the Edwards Sapien balloon (nominal, plus or minus). The goal of the valve is to make the calculation easy and quick. This should help the team calculate multiple scenarios quickly to arrive to a desirable sizing percentage. '),
@@ -100,8 +97,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             child: ElevatedButton(
               child: Text("Accept"),
               onPressed: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => CalculateScreen()));
+                Navigator.of(context)
+                    .pushReplacement(MaterialPageRoute(builder: (context) => CalculateScreen()));
               },
             ),
           ),
